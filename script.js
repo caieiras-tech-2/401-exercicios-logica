1fde92ad5206b44c39c0b434f7098414b5273fc0
Lucas Melo <luucasmell@gmail.com>
2019-07-23 17:51:40 -0300

    Adicionar resolucao dos exercicios

---
+++ script.js
@@ -0,0 +1,41 @@
+// // EXERCICIO EXTRA
+// let numeroCelular = prompt("Digite um número de celular válido");
+
+// if(numeroCelular.length == 9 && numeroCelular[0] === "9"){
+//     console.log("Número válido");
+// }
+// else{
+//     console.log("Número inválido");
+// }
+// idade maior que 60 e aposentados = gratis;
+// de 0 a 12 - 10 Reais;
+// de 13 a 17 - 15 reais ;
+// 18 a 59 - 35 reais;
+// se tiver de 18 a 59 e for estudante = meia entrada, 17,50
+
+// let nome = prompt(`Digite seu nome: `);
+// let idade = prompt(`${nome}, digite sua idade: `);
+// let aposentado = prompt(`Você está aposentado? S ou N`);
+
+// if(idade >= 60 || aposentado == 'S'){
+//     alert(`${nome}, olha só, entrada grátis !`);
+// }
+// else if(idade >= 0 && idade<=12){
+//     alert(`Olha ${nome}, a entrada não é gratis mas paga só 10zão`);
+// }
+// else if(idade >= 13 && idade <=17){
+//     alert(`Agora é um pouco mais caro ${nome}, mas custa só R$15,00`);
+// }
+// else if(idade >= 18 && idade<=59){
+//     let estudante = prompt(`${nome} você possui carteirinha de estudante? S ou N`);
+//     if(estudante == 'S'){
+//         console.log(`Ora ora, apenas R$17,50 em ${nome}.`);
+//     }
+//     else{
+//         console.log(`Vixi, bem caro. R$35,00 ${nome}`);
+//     }
+// }
+// else{
+//     console.log(`ERRO Idade inválida`);
+// }
+
